fe3994d6b55f97025069ccf59fd4d7cd96e5b0fd
KosBow <kosrat_95@hotmail.com>
2024-09-01 14:41:50 +0200

    One more 123

--- Injection_Test2/obj/Debug/net8.0-windows/Injection_Test2.AssemblyInfo.cs
+++ Injection_Test2/obj/Debug/net8.0-windows/Injection_Test2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Injection_Test2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93aa5063282bfe41802651ec6a9ec1ab0450d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1f67a31582c14dd649d812bbe19d6efcbbe8517")]
 [assembly: System.Reflection.AssemblyProductAttribute("Injection_Test2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Injection_Test2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
